db8a501733591f82b5b7682deae7f16cd27de4f9
undivisible <vms.fang.vortexmemesquad@gmail.com>
2023-10-19 11:43:50 +1100

    just better

--- botdivisible.ts
+++ botdivisible.ts
@@ -1,73 +1,132 @@
-import { Client, GatewayIntentBits, VoiceChannel } from 'discord.js';
-import * as ytdl from 'ytdl-core';
-import { StreamDispatcher, VoiceConnection } from 'discord.js';
-import { Message } from 'discord.js';
+import { Client, GatewayIntentBits } from 'discord.js';
+import { joinVoiceChannel, createAudioPlayer, createAudioResource, AudioPlayer, AudioResource, NoSubscriberBehavior, VoiceConnection } from '@discordjs/voice';
+import ytdl from 'ytdl-core';
+
+const queue: AudioResource[] = [];
+let player: AudioPlayer;
+let connection: VoiceConnection;
 
 const client = new Client({
-    intents: [
-        GatewayIntentBits.Guilds,
-        GatewayIntentBits.GuildMessages,
-        GatewayIntentBits.GuildVoiceStates,
-    ],
+	intents: [
+		GatewayIntentBits.Guilds,
+		GatewayIntentBits.GuildMessages,
+		GatewayIntentBits.GuildVoiceStates
+	]
 });
 
-const TOKEN = 'YOUR_BOT_TOKEN';
-const PREFIX = '/';
+const greetings: {[lang: string]: string[]} = {
+	en: ['hello', 'hi', 'hey', 'wassup', 'whats up', 'good morning', 'good afternoon', 'good day'],
+	ru: ['привет', 'здравствуйте', 'здравствуй'],
+	cn: ['早上好', '下午好', '晚安', '你好']
+};
 
-client.once('ready', () => {
-    console.log(`Logged in as ${client.user?.tag}!`);
-    client.user?.setActivity('you.', { type: 'WATCHING' });
-});
+client.on('messageCreate', async message => {
 
-client.on('messageCreate', async (message: Message) => {
-    if (message.author.bot) return;
+	if (message.author.bot) return;
 
-    const content = message.content.toLowerCase();
+	const content = message.content.toLowerCase();
 
-    // Define a dictionary to map greetings in different languages
-    const greetings: { [key: string]: string[] } = {
-        'en': ['hello', 'hi', 'hey', 'wassup', 'whats up', 'good morning', 'good afternoon', 'good day', 'wsg', 'gm'],
-        'ru': ['привет', 'здравствуйте', 'здравствуй', 'прив', 'здорово'],
-        'cn': ['早上好', '下午好', '晚安', '你好'],
-    };
+	for (const lang in greetings) {
+		for (const phrase of greetings[lang]) {
+			if (content.startsWith(phrase)) {
+				const name = message.author.username;
+				const randomGreeting = greetings[lang][Math.floor(Math.random() * greetings[lang].length)];
+				switch (name) {
+					case 'undivisible':
+						message.channel.send(`${randomGreeting}?`);
+						break;
+					case 'koolych':
+						message.channel.send(`${randomGreeting} koolych :333`);
+						break;
+					case 'leroy1107':
+						message.channel.send(`${randomGreeting} leroy`);
+						break;
+					case 'xavier8001':
+						message.channel.send(`${randomGreeting} xav`);
+						break;
+					case 'redmyfavcolour':
+						message.channel.send(`${randomGreeting} terence`);
+						break;
+				}
+			}
+		}
+	}
 
-    for (const lang in greetings) {
-        for (const phrase of greetings[lang]) {
-            if (content.startsWith(phrase)) {
-                const name = message.author.username;
-                const randomGreeting = greetings[lang][Math.floor(Math.random() * 5)];
-                switch (name) {
-                    case 'undivisible':
-                        message.channel.send(`${randomGreeting}?`);
-                        break;
-                    case 'koolych':
-                        message.channel.send(`${randomGreeting} koolych :333`);
-                        break;
-                    case 'leroy1107':
-                        message.channel.send(`${randomGreeting} leroy`);
-                        break;
-                    case 'xavier8001':
-                        message.channel.send(`${randomGreeting} xav`);
-                        break;
-                    case 'redmyfavcolour':
-                        message.channel.send(`${randomGreeting} terence`);
-                        break;
-                }
-            }
-        }
-    }
-
-    if (content.includes('french')) {
+	if (content.includes('french')) {
         message.channel.send('i think you mean fr*nch');
     } else if (content.includes('france')) {
         message.channel.send('i think you mean fr*nce');
     } else if (content.includes('dllm')) {
         message.channel.send('fuck you too');
     }
-
-    // Add your other message handling logic here...
-
+    
     if (message.author.id === '476637864814444545') {
+        switch (true) {
+            case content.startsWith('bot shut up'):
+                message.channel.send('ok whatever you say');
+                setTimeout(async () => {
+                    // Sleep for 10 minutes
+                    // Code here will execute after 10 minutes
+                }, 10 * 60 * 1000);
+                break;
+            case content.startsWith('good night'):
+                message.channel.send('good night leroy :))');
+                break;
+            case content.startsWith('gn'):
+                message.channel.send('gn :))');
+                break;
+            case content.startsWith('yes'):
+                message.channel.send('sure');
+                break;
+            case content.startsWith('thanks'):
+                message.channel.send('thanks');
+                break;
+        }
+    } else if (message.author.id === '581025257977020427') {
+        switch (true) {
+            case content.startsWith('bot shut up'):
+                message.channel.send('ok koolych :)). you\'re so cool');
+                setTimeout(async () => {
+                    // Sleep for 10 minutes
+                    // Code here will execute after 10 minutes
+                }, 10 * 60 * 1000);
+                break;
+            case content.startsWith('good night'):
+                message.channel.send('good night koolych i love you');
+                break;
+            case content.startsWith('gn'):
+                message.channel.send('gn :))');
+                break;
+            case content.startsWith('yes'):
+                message.channel.send('of course kooly');
+                break;
+            case content.startsWith('thanks'):
+                message.channel.send('thanksssss');
+                break;
+        }
+    } else if (message.author.id === '495178505605349376') {
+        switch (true) {
+            case content.startsWith('bot shut up'):
+                message.channel.send('no lol');
+                setTimeout(async () => {
+                    // Sleep for 10 minutes
+                    // Code here will execute after 10 minutes
+                }, 10 * 60 * 1000);
+                break;
+            case content.startsWith('good night'):
+                message.channel.send('good night xav');
+                break;
+            case content.startsWith('gn'):
+                message.channel.send('gn :))');
+                break;
+            case content.startsWith('yes'):
+                message.channel.send('no xavier, no');
+                break;
+            case content.startsWith('thanks'):
+                message.channel.send('no problem');
+                break;
+        }
+    } else if (message.author.id === '476637864814444545') {
         switch (true) {
             case content.startsWith('bot shut up'):
                 message.channel.send('fuck you');
@@ -100,52 +159,62 @@ client.on('messageCreate', async (message: Message) => {
         }
     }
 
-    // Add handling for other user IDs...
+	if (message.content.startsWith('/play')) {
+
+		const url = message.content.split(' ')[1];
+    
+		const connection = joinVoiceChannel({
+			channelId: message.member!.voice.channel!.id,
+			guildId: message.guild!.id,
+			adapterCreator: message.guild!.voiceAdapterCreator  
+		});
+
+		const stream = ytdl(url, { filter: 'audioonly' });
+
+		const resource = createAudioResource(stream);
+    
+		queue.push(resource);
+
+		if (!player) {
+			player = createAudioPlayer({
+				behaviors: {
+					noSubscriber: NoSubscriberBehavior.Pause
+				}
+			});
+			connection.subscribe(player);
+		}
+
+		player.play(resource);
+
+	}
+
+	if (message.content === '/pause') {
+		player?.pause();
+	}
+
+	if (message.content === '/skip') {
+		player?.stop();
+
+		if (queue.length > 0) {
+			const nextResource = queue.shift()!;
+			player?.play(nextResource);
+		}
+
+		if (queue.length === 0) {
+			connection.disconnect();
+		}
+	}
+
+	if (message.content === '/queue') {
+		message.channel.send(`Queue has ${queue.length} songs`);
+	}
 
 });
 
-// Voice handling
-let voiceConnection: VoiceConnection | null = null;
+client.on('debug', console.log);
 
-client.on('messageCreate', async (message: Message) => {
-    if (message.author.bot) return;
-
-    if (message.content.startsWith(`${PREFIX}play`)) {
-        const voiceChannel: VoiceChannel | null = message.member?.voice.channel;
-        if (!voiceChannel) {
-            message.channel.send('You are not in a voice channel!');
-            return;
-        }
-
-        const args: string[] = message.content.split(' ');
-        if (args.length !== 2 || !ytdl.validateURL(args[1])) {
-            message.channel.send('Please provide a valid YouTube URL.');
-            return;
-        }
-
-        if (voiceConnection) {
-            voiceConnection.join();
-        } else {
-            voiceConnection = await voiceChannel.join();
-        }
-
-        const stream = ytdl(args[1], { filter: 'audioonly' }); // Use 'ytdl' from 'ytdl-core-discord'
-        const dispatcher: StreamDispatcher = voiceConnection.play(stream);
-
-        dispatcher.on('finish', () => {
-            voiceConnection?.disconnect();
-        });
-
-        message.channel.send(`**Now playing:** ${args[1]}`);
-    } else if (message.content.startsWith(`${PREFIX}pause`)) {
-        if (voiceConnection) {
-            voiceConnection.voice?.connection?.dispatcher?.pause();
-            message.channel.send('Paused ⏸️');
-        } else {
-            message.channel.send('Not playing anything currently');
-        }
-    }
+client.on('ready', () => {
+  console.log(`bot connected!`);
 });
 
-// Log in with the bot's token
-client.login(TOKEN);
+client.login('MTE2MTg2NzkxNTAwNDQ4MTYwNg.G7oYNU.TY25ilP8AVwbmv_ie89eBRhrKACvXlvpGGFsCk');
